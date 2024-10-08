e3b9f09de4b792decfa7a617fe9992c23bbebf67
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-08 06:23:14 +0530

    Navbar highlight active route!

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import ContactPage from "./pages/ContactPage"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   </>
--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,55 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/** @format */
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
 
 const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
-      {/* Container wrapper */}
-      <div className="container-fluid">
-        {/* Toggle button */}
-        <button
-          data-mdb-collapse-init
-          className="navbar-toggler"
-          type="button"
-          data-mdb-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+			{/* Container wrapper */}
+			<div className="container-fluid">
+				{/* Toggle button */}
+				<button
+					data-mdb-collapse-init
+					className="navbar-toggler"
+					type="button"
+					data-mdb-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<i className="fas fa-bars"></i>
+				</button>
 
-        {/* Collapsible wrapper */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {/* Navbar brand */}
-          <Link className="navbar-brand mt-2 mt-lg-0" to="/">
-          <img 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRehbIRdC7RbRGTWVWQQxvHFXKCtBa0W6GjSQ&s" 
-        alt="Brand Logo" 
-        style={{ height: '80px' }} 
-    />          </Link>
-          {/* Navigation Links */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-            {/* If there's a non-navigational item, consider using a button */}
-            <li className="nav-item">
-              <button className="nav-link btn" onClick={() => alert('Action triggered!')}>
-                Action
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+				{/* Collapsible wrapper */}
+				<div
+					className="collapse navbar-collapse"
+					id="navbarSupportedContent">
+					{/* Navbar brand */}
+					<NavLink
+						className="navbar-brand mt-2 mt-lg-0"
+						to="/">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRehbIRdC7RbRGTWVWQQxvHFXKCtBa0W6GjSQ&s"
+							alt="Brand Logo"
+							style={{ height: "80px" }}
+						/>
+					</NavLink>
+
+					{/* Navigation Links */}
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								exact
+								to="/"
+								activeClassName="active">
+								Home
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								to="/about"
+								activeClassName="active">
+								About
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								to="/contact"
+								activeClassName="active">
+								Contact
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							{/* Removed the nav-link class and used a custom button class */}
+							<button
+								className="btn btn-action"
+								onClick={() => alert("Action triggered!")}>
+								Action
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
